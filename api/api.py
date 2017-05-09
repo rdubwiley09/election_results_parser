c0461bd2759b6ce9bb3aa8245e36d531bf4a668f
rdubwiley <wileyrya@gmail.com>
2017-05-08 20:51:26 -0400

    Added city function to api

--- api/api.py
+++ api/api.py
@@ -31,3 +31,18 @@ def results(year: hug.types.text, firstName: hug.types.text, lastName: hug.types
     resultQuery = "SELECT * FROM votes WHERE candidate_id = '%s';" %(str(candidateId))
     result = pd.read_sql(resultQuery,conn)
     return result.reset_index().to_json(orient="records")
+
+@hug.get(examples="year=2016&city=Southfield")
+def city(year: hug.types.text, city: hug.types.text):
+    """Returns the results for a given candidate for a given year"""
+    engine = create_engine(
+            'postgresql://%s:%s@%s/%s' %(user,pwd,ip,user),
+            client_encoding='utf8',echo=False)
+    conn = engine.connect()
+    Base = declarative_base()
+    query = "SELECT * FROM cities WHERE election_year = '%s' AND city_description = '%s CITY' OR city_description = '%s TOWNSHIP'" %(str(year),city.upper(),city.upper())
+    df = pd.read_sql(query, conn)
+    cityId = df['city_code'].tolist()[0]
+    resultQuery = "SELECT * FROM votes WHERE city_code = '%s';" %(str(cityId))
+    result = pd.read_sql(resultQuery,conn)
+    return result.reset_index().to_json(orient="records")
