3582bc6b72615c06a347cd902a73870a182d0fe3
rdubwiley <wileyrya@gmail.com>
2017-05-13 18:31:44 -0400

    Added vote percentages to results route

--- api/api.py
+++ api/api.py
@@ -30,7 +30,12 @@ def results(year: hug.types.text, firstName: hug.types.text, lastName: hug.types
     candidateId = df['candidate_id'].tolist()[0]
     resultQuery = "SELECT * FROM votes WHERE candidate_id = '%s';" %(str(candidateId))
     result = pd.read_sql(resultQuery,conn)
-    return result.reset_index().to_json(orient="records")
+    officeId, districtId = result['office_code'].tolist()[0], result['district_code'].tolist()[0]
+    totalQuery = "Select office_code, district_code, county_code, city_code, ward_number, precinct_number, SUM(precinct_votes) AS total_votes FROM votes WHERE office_code = '%s' AND district_code = '%s'  AND election_year = '%s' GROUP BY 1,2,3,4,5,6" %(str(officeId),str(districtId),str(year))
+    totalTable = pd.read_sql(totalQuery,conn)
+    output = pd.merge(result,totalTable, on = ['office_code', 'district_code', 'county_code', 'city_code', 'ward_number', 'precinct_number'], how="inner")
+    output['candidate_percentage'] = 100*output['precinct_votes']/output['total_votes']
+    return output.reset_index().to_json(orient="records")
 
 @hug.get(examples="year=2016&city=Southfield")
 def city(year: hug.types.text, city: hug.types.text):
